d9a6b31809182186e1619f598f784ee1fca5cbb8
moatasm <hajjo.dev@gmail.com>
2025-12-13 19:50:32 +0100

    work on skills

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -10,10 +10,5 @@ import {RouterModule} from '@angular/router';
   styleUrl: './about.css',
 })
 export class About {
-  stats = [
-    { value: '50+', label: 'Projects Completed' },
-    { value: '5+', label: 'Years Experience' },
-    { value: '20+', label: 'Happy Clients' },
-    { value: '100%', label: 'Satisfaction Rate' }
-  ];
+
 }
